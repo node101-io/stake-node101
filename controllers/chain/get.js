9260cf342e073864b279d5162078ae71179ed537
Kaleab Abayneh <kaleababayneh65@gmail.com>
2024-05-22 19:55:31 +0200

    fixed bug

--- controllers/chain/get.js
+++ controllers/chain/get.js
@@ -1,17 +1,5 @@
 const ChainInfo = require('../../models/ChainInfo/ChainInfo');
 
-/* function findChainByChainId (req, res) {
-  const chainId = req.query.chainid;
-
-  ChainInfo.findChainInfoByChainId(chainId, (error, chainInfo) => {
-      if (error) {
-        return res.json({ error: error });
-      }
-        return res.json({ chainInfo: chainInfo });
-    });
-}
-
-module.exports = findChainByChainId; */
 
 module.exports = (req, res) => {
   const chainId = req.query.chainId;
